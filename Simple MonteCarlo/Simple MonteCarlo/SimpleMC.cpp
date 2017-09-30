61a7dc941960c82a9a81bc43921fe19d2d732e89
Magnus Strandberg <magnus.jo.strandberg@gmail.com>
2017-09-30 14:36:17 +0300

    Getting somewhere

--- Simple MonteCarlo/Simple MonteCarlo/SimpleMC.cpp
+++ Simple MonteCarlo/Simple MonteCarlo/SimpleMC.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+void fileReader(string);
+
 
 int main() {
-	cout << "Hellooou";
+	cout << "Hellooou \n";
+	string filelocation = "C:\\Users\\Magnus\\source\\repos\\Simple_MonteCarlo\\Simple MonteCarlo\\Simple MonteCarlo\\input.txt";
+	fileReader (filelocation);
 
+	std::cin.ignore(); //So we can see shit
 	return 0;
 }
 
+void fileReader(string FileLocation) {
+	string line;
+	ifstream input(FileLocation);
+	if (input.is_open())
+	{
+		while (getline(input, line))
+		{
+			
+			if (!line.empty() && line.at(0) != '%')
+			{
+				cout << line << '\n';
+			}
+			
+		}
+		input.close();
+	}
 
+	else cout << "Unable to open file";
 
-void fileReader() {
-
-
-
+	return;
 }
